3f184e99ce390fb81d27dccf377e66ace6c221a1
Sungup Moon <sungup.moon@samsung.com>
2022-12-28 17:19:48 +0900

    types.Bytes: rebuild algorithm and data type
    - add test case builder
    
    Signed-off-by: Sungup Moon <sungup@me.com>

--- pkg/types/bytes.go
+++ pkg/types/bytes.go
@@ -12,20 +12,11 @@ var (
 	byteFormat = regexp.MustCompile("^([0-9]+(|\\.[0-9]*))[[:space:]]*([KMGTP]iB|[KMGTP]B|B|)$")
 )
 
-type unit uint64
-
 const (
-	B = unit(1 << (10 * iota))
-	KiB
-	MiB
-	GiB
-	TiB
-	PiB
-
 	unitStr = "BKMGTP"
 )
 
-func (u *unit) Parse(text string) (err error) {
+func ParseUnit(text string) (size uint64, err error) {
 	text = strings.TrimSpace(text)
 	if unitRegex.MatchString(text) {
 		var ch byte
@@ -38,63 +29,44 @@ func (u *unit) Parse(text string) (err error) {
 
 		for i, v := range []byte(unitStr) {
 			if v == ch {
-				*u = unit(1 << (10 * i))
-				return nil
+				return uint64(1) << (10 * i), nil
 			}
 		}
 	}
 
-	return fmt.Errorf("unexpected size format: \"%v\"", text)
+	return 0, fmt.Errorf("unexpected size format: \"%v\"", text)
 }
 
-func (u *unit) Size() uint64 {
-	return uint64(*u)
-}
+type Bytes uint64
 
-func (u *unit) String() string {
-	if *u == B {
-		return "B"
-	}
-
-	for i, shift := 1, 10; i < len(unitStr); i, shift = i+1, shift+10 {
-		if uint64(*u) == uint64(1)<<shift {
-			return string(unitStr[i]) + "iB"
-		}
-	}
-
-	return "NaN"
-}
-
-type Byte struct {
-	bytes  uint64
-	number float64
-	unit   unit
-}
-
-func (b *Byte) Parse(text string) (err error) {
+func (b *Bytes) Parse(text string) (err error) {
 	var (
-		n float64
-		u unit
+		number float64
+		unit   uint64
+		form   []string
 	)
 
-	form := byteFormat.FindStringSubmatch(text)
+	if form = byteFormat.FindStringSubmatch(strings.TrimSpace(text)); len(form) == 0 {
+		return fmt.Errorf("invalid byte string format")
+	}
 
-	if n, err = strconv.ParseFloat(form[1], 64); err == nil {
-		if err = u.Parse(form[len(form)-1]); err == nil {
-			b.number = n
-			b.unit = u
-
-			b.bytes = uint64(b.number * float64(b.unit.Size()))
+	if number, err = strconv.ParseFloat(form[1], 64); err == nil {
+		if unit, err = ParseUnit(form[len(form)-1]); err == nil {
+			*b = Bytes(uint64(number * float64(unit)))
 		}
 	}
 
 	return err
 }
 
-func (b *Byte) String() string {
-	return strconv.FormatFloat(b.number, 'f', 3, 64) + " " + b.unit.String()
-}
+func (b *Bytes) String() string {
+	const max = len(unitStr) - 1
+	for i, unit := max, uint64(1)<<max; 0 < i; i, unit = i-1, unit>>10 {
+		if unit < uint64(*b) {
+			number := float64(*b) / float64(unit)
+			return strconv.FormatFloat(number, 'f', 3, 64) + " " + string(unitStr[i-1]) + "iB"
+		}
+	}
 
-func (b *Byte) Uint64() uint64 {
-	return b.bytes
+	return "B"
 }
--- pkg/types/bytes_test.go
+++ pkg/types/bytes_test.go
@@ -1,66 +1,104 @@
 package types
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"strconv"
+	"strings"
 	"testing"
 )
 
-func TestUnit_Parse(t *testing.T) {
-	assert.Fail(t, "not yet implemented")
+var (
+	tcNumbers = []float64{25.5, 10.0, 15.0, 1.0, 0.5, 0.25}
+
+	tcValidUnitFormat     map[string]uint64
+	tcInvalidUnitFormat   []string
+	tcValidNumberFormat   map[string]uint64
+	tcInvalidNumberFormat []string
+)
+
+func makePadding(text string) []string {
+	return []string{text, text + " ", " " + text, " " + text + " "}
 }
 
-func TestUnit_Size(t *testing.T) {
-	assert.Fail(t, "not yet implemented")
+func init() {
+	tcValidUnitFormat = map[string]uint64{"": 1, "B": 1}
+	tcInvalidUnitFormat = []string{"invalid", "b"}
+	tcValidNumberFormat = make(map[string]uint64)
+	tcInvalidNumberFormat = []string{"", "B"}
+
+	for _, n := range tcNumbers {
+		strNumber := fmt.Sprintf("%f", n)
+		for _, key := range makePadding(strNumber) {
+			tcValidNumberFormat[key] = uint64(n)
+
+			for _, tail := range makePadding("B") {
+				tcValidNumberFormat[key+tail] = uint64(n)
+			}
+		}
+
+		tcInvalidNumberFormat = append(tcInvalidNumberFormat, strings.ToLower(strNumber+"B"))
+	}
+
+	for i, v := range unitStr[1:] {
+		unit := string(v)
+		size := uint64(1) << ((i + 1) * 10)
+
+		for _, tail := range []string{"iB", "B"} {
+			tcValidUnitFormat[unit+tail] = size
+			tcValidUnitFormat[unit+tail+" "] = size
+			tcValidUnitFormat[" "+unit+tail] = size
+			tcValidUnitFormat[" "+unit+tail+" "] = size
+		}
+		tcInvalidUnitFormat = append(tcInvalidUnitFormat, strings.ToLower(string(v)+"iB"), strings.ToLower(string(v)+"B"))
+
+		for _, n := range tcNumbers {
+			strNumber := fmt.Sprintf("%f", n)
+			for _, number := range makePadding(strNumber) {
+				for _, form := range []string{"iB", "B"} {
+					for _, tail := range makePadding(unit + form) {
+						tcValidNumberFormat[number+tail] = uint64(n * float64(size))
+						tcInvalidNumberFormat = append(tcInvalidUnitFormat, strings.ToLower(number+tail))
+					}
+				}
+			}
+		}
+	}
 }
 
-func TestUnit_String(t *testing.T) {
-	assert.Fail(t, "not yet implemented")
+func TestParseUnit(t *testing.T) {
+	for in, expected := range tcValidUnitFormat {
+		generated, err := ParseUnit(in)
+		assert.NoError(t, err)
+		assert.Equal(t, expected, generated)
+	}
+
+	for _, in := range tcInvalidUnitFormat {
+		generated, err := ParseUnit(in)
+		assert.Error(t, err)
+		assert.Zero(t, generated)
+	}
 }
 
-func TestByte_Parse(t *testing.T) {
-	tested := Byte{}
+func TestBytes_Parse(t *testing.T) {
+	for in, expected := range tcValidNumberFormat {
+		var tested Bytes
+		assert.NoError(t, tested.Parse(in), in)
+		assert.Equal(t, expected, uint64(tested))
+	}
 
-	assert.NoError(t, tested.Parse("1"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1."))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1.1"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1B"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1B"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1.0B"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1 B"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1.1 B"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1GB"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1.1GB"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1 GB"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1.1 GB"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1GiB"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1.1GiB"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1 GiB"))
-	t.Log(tested.String())
-	assert.NoError(t, tested.Parse("1.1 GiB"))
-	t.Log(tested.String())
-
-	assert.Fail(t, "not yet implemented")
-
+	for _, in := range tcInvalidNumberFormat {
+		var tested Bytes
+		assert.Error(t, tested.Parse(in))
+		assert.Zero(t, uint64(tested))
+	}
 }
 
-func TestByte_String(t *testing.T) {
-	assert.Fail(t, "not yet implemented")
-}
-
-func TestByte_Uint64(t *testing.T) {
+func TestBytes_String(t *testing.T) {
+	for sample, value := range tcValidNumberFormat {
+		tested := Bytes(value)
+		in, err := strconv.ParseFloat(strings.Trim(sample, " iB"+unitStr), 64)
+		t.Logf("result: %v | in: %v | err: %v", tested.String(), in, err)
+	}
 	assert.Fail(t, "not yet implemented")
 }
